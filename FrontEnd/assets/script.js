088636d52e7eb05ef2f338c1f43e610a39d473fa
Mickael Bouillez <bouillezmickael62@gmail.com>
2023-12-13 13:24:53 +0100

    Creation lien et style filtres + page login full faite

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,44 +1,113 @@
-document.addEventListener("DOMContentLoaded", function () { //Permet de charger le DOM avant l'execution du script//
-    const worksGallery = document.querySelector(".gallery"); //Sélectionne l'élément avec la classe "gallery"
+document.addEventListener("DOMContentLoaded", function () {
+    const worksGallery = document.querySelector(".gallery");
+    const listFilter = document.querySelector(".filters");
 
-    fetch('http://localhost:5678/api/works') //Effectue une requête vers l'API pour récupérer les données
+    // Fonction pour filtrer les images par catégorie
+    function filterImagesByCategory(categoryName) {
+        const allWorkItems = document.querySelectorAll('.work-item');
+
+        allWorkItems.forEach(workItem => {
+            const workItemCategoryName = workItem.dataset.categoryName;
+
+            if (categoryName === "all" || workItemCategoryName === categoryName) {
+                workItem.style.display = "block";
+            } else {
+                workItem.style.display = "none";
+            }
+        });
+    }
+
+    // Récupère les œuvres depuis l'API
+    fetch('http://localhost:5678/api/works')
         .then(response => {
-            if (!response.ok) { // Vérifie si la réponse est OK, sinon génère une erreur
+            if (!response.ok) {
                 throw new Error('La réponse du réseau n\'était pas correcte. Code de statut : ' + response.status);
             }
             return response.json();
         })
         .then(data => {
-            // Maintenant, 'data' contient la réponse du serveur
-            console.log(data);
-
-            // Creation du lieu de reception des données
+            // Parcourt chaque œuvre dans les données récupérées
             data.forEach(item => {
-                // Créez un conteneur pour chaque élément
+                // Crée un conteneur pour chaque œuvre
                 const workContainer = document.createElement('div');
                 workContainer.classList.add('work-item');
+                workContainer.dataset.categoryName = item.category.name;
 
-                // Créez une image pour chaque élément
+                // Crée une balise img pour afficher l'image
                 const workImage = document.createElement('img');
-                workImage.src = item.imageUrl; ///!\ propriété imageURL obligatoire
+                workImage.src = item.imageUrl;
                 workImage.alt = item.title;
 
-                // Créez un titre pour chaque élément
+                // Crée un titre pour l'œuvre
                 const workTitle = document.createElement('div');
                 workTitle.classList.add('work-title');
-                workTitle.textContent = item.title; ///!\ propriété title obligatoire
+                workTitle.textContent = item.title;
 
-                // Ajoutez l'image et le titre au conteneur
+                // Ajoute l'image et le titre au conteneur
                 workContainer.appendChild(workImage);
                 workContainer.appendChild(workTitle);
 
-                // Ajoutez le conteneur à la galerie
+                // Ajoute le conteneur à la galerie
                 worksGallery.appendChild(workContainer);
             });
         })
         .catch(error => {
-            console.error('Erreur lors de la récupération des données :', error); // En cas d'erreur, affiche un message dans la console
+            console.error('Erreur lors de la récupération des données :', error);
+        });
+
+    // Récupère les catégories depuis l'API
+    fetch('http://localhost:5678/api/categories')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('La réponse du réseau n\'était pas correcte. Code de statut : ' + response.status);
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Ajout du bouton "Tous" avec un gestionnaire d'événements
+            const allFilterButton = document.createElement('button');
+            allFilterButton.classList.add('filter-item', 'all-button');
+            const allFilterName = document.createElement('span');
+            allFilterName.classList.add('filter-name');
+            allFilterName.textContent = 'Tous';
+
+            allFilterButton.appendChild(allFilterName);
+            listFilter.prepend(allFilterButton);  // Utilisez prepend pour placer le bouton "Tous" en premier
+
+            allFilterButton.addEventListener('click', () => {
+                filterImagesByCategory('all');
+            });
+
+            // Parcourt chaque catégorie dans les données récupérées
+            data.forEach(item => {
+                // Crée un bouton de filtre
+                const filterButton = document.createElement('button');
+                filterButton.classList.add('filter-item');
+
+                // Crée un span pour le texte du bouton
+                const filterName = document.createElement('span');
+                filterName.classList.add('filter-name');
+                filterName.textContent = item.name;
+
+                // Ajoute le texte au bouton
+                filterButton.appendChild(filterName);
+                // Ajoute le bouton à la liste de filtres
+                listFilter.appendChild(filterButton);
+
+                // Ajoute un gestionnaire d'événements pour détecter les clics sur le bouton de filtre
+                filterButton.addEventListener('click', () => {
+                    // Récupère le nom de la catégorie associée au bouton de filtre cliqué
+                    const selectedCategoryName = item.name;
+                    // Appelle la fonction de filtrage avec le nom de la catégorie
+                    filterImagesByCategory(selectedCategoryName);
+                });
+            });
+        })
+        .catch(error => {
+            console.error('Erreur lors de la récupération des catégories:', error);
         });
 });
+
+
 
 
