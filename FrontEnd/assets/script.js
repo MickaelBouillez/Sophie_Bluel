0880259cf5c2739e44d14a488e9c958ebf9e06f1
Mickael Bouillez <bouillezmickael62@gmail.com>
2024-01-04 11:19:48 +0100

    avancement modale

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -85,11 +85,13 @@ document.addEventListener("DOMContentLoaded", function () {
     fetchWorks();
     fetchCategories();
 
+
     // Récupère le bouton de connexion
     const loginButton = document.getElementById('loginButton');
 
     // Vérifier l'état de connexion au chargement de la page
     const isLoggedIn = localStorage.getItem('LogOk');
+
     if (isLoggedIn === 'true') {
         // Si l'utilisateur est connecté, masquer le formulaire de connexion
         document.querySelector('.login').style.display = 'none';
@@ -98,7 +100,9 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     // Fetch avec la methode post ( JSON.stringify())
-    document.querySelector('.login form').addEventListener("submit", async function (event) {
+    const loginform = document.getElementById(".loginform");
+
+    loginform.addEventListener("submit", async function (event) {
         event.preventDefault(); // Empêcher le rechargement de la page
 
         const isLoggedIn = localStorage.getItem('LogOk');
@@ -117,42 +121,50 @@ document.addEventListener("DOMContentLoaded", function () {
         const email = document.querySelector("#email");
         const password = document.querySelector("#password");
 
-        await fetch("http://localhost:5678/api/users/login", {
-            method: "POST",
-            headers: {
-                accept: "application/JSON",
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                email: email.value,
-                password: password.value,
-            }),
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                let token = data.token;
-
-                if (token) {
-                    console.log("Connexion ok");
-                    window.location.href = "index.html";
-                    localStorage.setItem('LogOk', 'true');
-                    localStorage.setItem('token', token); // Stocker le token
-                    // Mettre à jour le texte du bouton
-                    loginButton.textContent = 'Logout';
-                } else {
-                    console.log("Non connecté");
-                    afficherMessageErreur("Erreur de connexion. Veuillez vérifier vos identifiants.");
-                }
-            })
-            .catch((error) => {
-                console.error("Erreur", error);
-                afficherMessageErreur("Une erreur inattendue s'est produite. Veuillez réessayer.");
+        try {
+            const response = await fetch("http://localhost:5678/api/users/login", {
+                method: "POST",
+                headers: {
+                    accept: "application/JSON",
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    email: email.value,
+                    password: password.value,
+                }),
             });
+
+            if (!response.ok) {
+                console.error("Erreur lors de la requête:", response.status, response.statusText);
+                afficherMessageErreur("Une erreur inattendue s'est produite. Veuillez réessayer.");
+                return;
+            }
+
+            const data = await response.json();
+            let token = data.token;
+
+            if (token) {
+                console.log("Connexion ok");
+                window.location.href = "index.html";
+                localStorage.setItem('LogOk', 'true');
+                localStorage.setItem('token', token); // Stocker le token
+                // Mettre à jour le texte du bouton
+                loginButton.textContent = 'Logout';
+            } else {
+                console.log("Non connecté");
+                afficherMessageErreur("Erreur de connexion. Veuillez vérifier vos identifiants.");
+            }
+        } catch (error) {
+            console.error("Erreur", error);
+            afficherMessageErreur("Une erreur inattendue s'est produite. Veuillez réessayer.");
+        }
     }
 
+    document.querySelector('.login-button').addEventListener("click", logout);
     // Fonction pour gérer la déconnexion
     function logout() {
         // Supprimer l'état de connexion et le token du localStorage
+        console.log('logout');
         localStorage.removeItem('LogOk');
         localStorage.removeItem('token');
 
@@ -165,132 +177,21 @@ document.addEventListener("DOMContentLoaded", function () {
         badLogin.style.display = "flex";
         badLogin.textContent = message;
     }
-});
 
-//////////////////////////////////////////////mode édition
-
-/* Fonction pour générer le mode édition
-function genererEdit() {
-    editorMode.style.display = "none";
-    modal.style.display = "none";
-    if (token) {
-        LogIn.style.display = "none";
-        editorMode.style.display = "flex";
-        divCategory.style.display = "none";
-        Modifier.style.display = "flex";
-        console.log("Mode éditeur");
-    } else {
-        LogOut.style.display = "none";
+    // Vérifier l'état de connexion au chargement de la page
+    if (isLoggedIn !== 'true') {
+        // Si l'utilisateur est connecté, masquer le mode edition
+        document.querySelector('.edition').style.display = 'none';
+        document.querySelector('.editproject').style.display = 'none';
     }
-}
-genererEdit();
+    console.log(isLoggedIn);
 
-// Gestion de la déconnexion
-LogOut.addEventListener("click", function disconnect() {
-    console.log("Déconnecté avec succès");
-    LogIn.style.display = "block";
-    LogOut.style.display = "none";
-    editorMode.style.display = "none";
-    divCategory.style.display = "flex";
-    Modifier.style.display = "none";
-    localStorage.clear();
-    window.location.reload();
-});
+    //affichage modale
+    const containerModals = document.querySelector(".containerModals")
+    const edition = document.querySelector(".edition");
 
-// Génération du contenu de la modale pour les œuvres
-function genererModalWorks(works) {
-    const galleryModal = document.querySelector(".galleryModal");
-    galleryModal.innerHTML = "";
-
-    if (token) {
-        for (const modalWork of works) {
-            const modalContainer = document.createElement("figure");
-            modalContainer.classList.add("imgWidth");
-
-            // Création de la div d'image
-            const divImgAdd = document.createElement("div");
-            divImgAdd.classList.add("div_img_add");
-
-            // Création de l'image
-            const modalImg = document.createElement("img");
-            modalImg.src = modalWork.imageUrl;
-
-            // Création du titre d'édition
-            const modalImgTitle = document.createElement("figcaption");
-            modalImgTitle.innerText = "éditer";
-            modalImgTitle.classList.add("editText");
-
-            // Création du bouton de suppression
-            const deleteButton = document.createElement("button");
-            deleteButton.classList.add("poubelle");
-            deleteButton.innerHTML = '<i class="fa-regular fa-trash-can modal_trash-icon"></i>';
-
-            // Suppression d'une œuvre
-            deleteButton.addEventListener("click", (e) => {
-                e.preventDefault();
-                console.log(modalWork.id);
-                let request = {
-                    method: "DELETE",
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                };
-                fetch("http://localhost:5678/api/works/" + modalWork.id, request)
-                    .then((response) => {
-                        if (response.ok) {
-                            console.log("Suppression réussie");
-                            init();
-                        } else {
-                            console.log("Échec de la suppression");
-                        }
-                    })
-                    .catch((error) => {
-                        console.error("Échec", error);
-                    });
-            });
-        }
-    }
-}
-
-// Ouvrir la modale
-edit_button.addEventListener("click", function openModal() {
-    // Affiche la modale d'édition et masque la modale d'ajout
-    modal.style.display = "flex";
-    modal_edit.style.display = "flex";
-    modal_add.style.display = "none";
-    console.log(token);
-    // Variable non utilisée - à revoir si nécessaire
-    edit_button = true;
-});
-
-// Fermer la modale
-const closeModalButtons = document.querySelectorAll('.close_Modal button');
-
-closeModalButtons.forEach(button => {
-    button.addEventListener('click', function () {
-        modal.style.display = 'none';
-        information.style.display = 'none';
-        console.log("Fermeture");
-        // Réinitialisez l'état de la modale si nécessaire
-        edit_button = false;
-    });
-});
-
-// Retour en arrière
-return_back.addEventListener("click", function returnBack() {
-    modal_edit.style.display = "none";
-    modal_add.style.display = "none"; // Ajoutez cette ligne pour masquer la modale d'ajout également
-    information.style.display = "none";
-    resetForm();
-});
-
-// Ajout de photo
-add_picture.addEventListener("click", function addPicture() {
-    // Masque la modale d'édition et affiche la modale d'ajout
-    modal_edit.style.display = "none";
-    modal_add.style.display = "flex";
-    // Initialise le formulaire d'ajout
-    addModal();
-});
-*/
-
+    edition.addEventListener("click", () => {
+        console.log("edition");
+        containerModals.style.display = "flex";
+    })
+})
